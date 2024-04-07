47c86b0855c1c3fafea99d9cfa2c2bc20481f496
Chandan Kumar Nayak <107075048+MrProgrammerr@users.noreply.github.com>
2024-04-07 10:16:16 +0530

    First

---
+++ app.py
@@ -0,0 +1,123 @@
+import streamlit as st
+import preprocessor
+import helper
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+st.sidebar.title("Whatsapp Chat Analyzer")
+uploaded_file = st.sidebar.file_uploader("Choose a file")
+if uploaded_file is not None:
+    bytes_data = uploaded_file.getvalue()
+    data = bytes_data.decode("utf-8")
+    df = preprocessor.preprocess(data)
+
+    # Fetch unique users
+    user_list = df['User'].unique().tolist()
+    user_list.remove('notification')
+    user_list.sort()
+    user_list.insert(0,"Total Group")
+    selected_user = st.sidebar.selectbox("Select User : ",user_list)
+
+    if st.sidebar.button("Analyze") :
+
+        # Stats Area
+        st.title("Top Stats")
+        num_msgs,words,medias,links = helper.fetch_stats(selected_user,df)
+        col1,col2,col3,col4 = st.columns(4)
+        with col1:
+            st.header("Total Messages")
+            st.title(num_msgs)
+        with col2:
+            st.header("Total Words")
+            st.title(len(words))
+        with col3:
+            st.header("Total Medias")
+            st.title(medias)
+        with col4:
+            st.header("Total Links")
+            st.title(len(links))
+
+        # Time Wise analysis
+        st.title("Time vs Messages")
+        time_df = helper.analyze_with_time(selected_user,df)
+        fig,ax = plt.subplots()
+        ax.plot(time_df['Time'],time_df['Message'])
+        plt.xticks(rotation='vertical')
+        st.pyplot(fig)
+
+        # activity map
+        st.title('Activity Map')
+        col1, col2 = st.columns(2)
+
+        with col1:
+            st.header("Most busy day")
+            busy_day = helper.week_activity_map(selected_user, df)
+            fig, ax = plt.subplots()
+            ax.bar(busy_day.index, busy_day.values, color='purple')
+            plt.xticks(rotation='vertical')
+            st.pyplot(fig)
+
+        with col2:
+            st.header("Most busy month")
+            busy_month = helper.month_activity_map(selected_user, df)
+            fig, ax = plt.subplots()
+            ax.bar(busy_month.index, busy_month.values, color='orange')
+            plt.xticks(rotation='vertical')
+            st.pyplot(fig)
+
+        st.title("Weekly Activity Map")
+        user_heatmap = helper.activity_heatmap(selected_user, df)
+        fig, ax = plt.subplots()
+        ax = sns.heatmap(user_heatmap)
+        st.pyplot(fig)
+
+        # Finding Busiest Users
+        if selected_user == "Total Group":
+            st.title("Busy Users")
+            highest, percentage_df = helper.most_busy_users(df)
+            lowest = helper.least_busy_users(df)
+            col1,col2 = st.columns(2)
+            with col1:
+                st.header("Most Busy")
+                fig, axes = plt.subplots()
+                axes.bar(highest.index, highest.values, color='red')
+                plt.xticks(rotation='vertical')
+                st.pyplot(fig)
+            with col2:
+                st.header("Least Busy")
+                fig, axes = plt.subplots()
+                axes.bar(lowest.index, lowest.values, color='red')
+                plt.xticks(rotation='vertical')
+                st.pyplot(fig)
+            col1, col2 = st.columns(2)
+            with col1:
+                st.header("Percentages")
+                st.dataframe(percentage_df)
+            with col2:
+                st.header('Pie Chart')
+                fig, axes = plt.subplots()
+                axes.pie(percentage_df.head(10).Percentage, labels=percentage_df.head(10).User, startangle=140, counterclock=False)
+                plt.xticks(rotation='vertical')
+                st.pyplot(fig)
+        wc = helper.create_wordcloud(selected_user,df)
+        fig,axes = plt.subplots()
+        axes.imshow(wc)
+        plt.axis('off')
+        st.title("Words Used : ")
+        st.pyplot(fig)
+
+        st.title("Most Common Words : ")
+        most_common_words = helper.most_commom_words(selected_user,df)
+        fig, ax = plt.subplots()
+        ax.barh(most_common_words[0], most_common_words[1])
+        st.pyplot(fig)
+
+        st.title("Emoji Analysis :")
+        emjdf = helper.emoji_func(selected_user,df)
+        col1,col2 = st.columns(2)
+        with col1:
+            st.dataframe(emjdf)
+        with col2:
+            fig,ax = plt.subplots()
+            ax.barh(emjdf[0].head(20),emjdf[1].head(20))
+            st.pyplot(fig)
---
+++ helper.py
@@ -0,0 +1,93 @@
+import pandas as pd
+from urlextract import URLExtract
+from wordcloud import WordCloud
+import emoji
+from collections import Counter
+def fetch_stats(selected_user,df) :
+    if selected_user != "Total Group":
+        df = df[df['User'] == selected_user]
+
+    # Fetch Number of Messages
+    num_msgs = df.shape[0]
+
+    # Find All tDe wNrds
+    words = []
+    for msg in df['Message']:
+        words.extend(msg.split())
+
+    # Find Number of Media Files
+    medias = df[df['Message']=='<Media omitted>\n'].shape[0]
+
+    # Find Number of Links
+    links = []
+    extractor = URLExtract()
+    for msg in df['Message']:
+        links.extend(extractor.find_urls(msg))
+
+    return num_msgs, words, medias,links
+def most_busy_users(df):
+    chat_percentage = round(df['User'].value_counts() / df.shape[0], 3).reset_index()
+    chat_percentage.columns = ['User', 'Percentage']
+    return df['User'].value_counts().head(), chat_percentage
+def least_busy_users(df) :
+    return df['User'].value_counts().tail()
+
+def create_wordcloud(selected_user, df):
+    if selected_user != "Total Group":
+        df = df[df['User'] == selected_user]
+    df = df[df['Message'] != '<Media omitted>\n']
+    wc = WordCloud(width=500, height=500, min_font_size=10, background_color='white')
+    df_wc = wc.generate(df['Message'].str.cat(sep=" "))
+    return df_wc
+def most_commom_words(selected_user, df):
+    if selected_user != "Total Group":
+        df = df[df['User'] == selected_user]
+    pure_df = df[df['User'] != 'notification']
+    pure_df = pure_df[pure_df['Message'] != '<Media omitted>\n']
+    f = open('C:/Users/cn807/OneDrive/Desktop/ML And DL/ML_AND_DL/My_Projects/Whatsapp Chat Analyzer/stop_hinglish.txt', 'r', encoding='utf-8')
+    stop_words = f.read()
+    stop_words = stop_words.split('\n')
+    words = []
+    for msg in pure_df['Message']:
+        for word in msg.lower().split():
+            if (word not in stop_words and len(word)!=1):
+                words.append(word)
+    return pd.DataFrame(Counter(words).most_common(20))
+def emoji_func(selected_user, df):
+    if selected_user != "Total Group":
+        df = df[df['User'] == selected_user]
+    emojis = []
+    for msg in df['Message']:
+        emojis.extend([c for c in msg if c in emoji.UNICODE_EMOJI['en']])
+    emojidf = pd.DataFrame(Counter(emojis).most_common(len(Counter(emojis))))
+    return emojidf
+
+def analyze_with_time(selected_user , df):
+    if selected_user != "Total Group":
+        df = df[df['User'] == selected_user]
+    time_df = df.groupby(['Year', 'Month', 'Month_Num']).count()['Message'].reset_index()
+    time_df = time_df.sort_values(['Year', 'Month_Num'])
+    time_df = time_df.reset_index(drop=True)
+    times = []
+    for i in range(len(time_df)):
+        times.append(str(time_df['Year'][i]) + '-' + str(time_df['Month'][i]))
+    time_df['Time'] = times
+    return time_df
+
+def week_activity_map(selected_user,df):
+    if selected_user != "Total Group":
+        df = df[df['User'] == selected_user]
+    return df['Day_Name'].value_counts()
+
+def month_activity_map(selected_user,df):
+    if selected_user != "Total Group":
+        df = df[df['User'] == selected_user]
+    return df['Month'].value_counts()
+
+
+def activity_heatmap(selected_user,df):
+    if selected_user != "Total Group":
+        df = df[df['User'] == selected_user]
+    user_heatmap = df.pivot_table(index='Day_Name', columns='Period', values='Message', aggfunc='count').fillna(0)
+
+    return user_heatmap
